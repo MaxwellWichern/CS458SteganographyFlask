b4adb3238280aa4b2f8af57191de22cc6c111840
Gabe <trebelhorng2865@my.uwstout.edu>
2023-10-04 15:54:02 -0500

    message

--- blueprints/demo/views.py
+++ blueprints/demo/views.py
@@ -6,6 +6,8 @@ import functions as fun
 def home():
     # mess = fun.s3Upload('requirements.txt', 'big.txt')
     # print(mess)
+    # mess = fun.s3Delete('stegosaurus', 'big.txt')
+    # print(mess)
     return "Hello world!"
 
 @app.route('/hello/<name>', methods=['GET'])
@@ -14,7 +16,10 @@ def user(name):
 
 @app.route('/post', methods=['POST'])
 def testPost():
-    data = request.get_json()
+    data = request.get_data()
+    print(data)
+    # data = request.get_json()
+    # print(data)
     return data
 
 @app.route('/<num1>/<num2>')
